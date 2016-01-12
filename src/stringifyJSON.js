106f4524539d501d9728b0054760e3b7d4ef0841
Alex Ko <alexko@utexas.edu>
2016-01-11 17:45:05 -0700

    implemented stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,44 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function(obj){
+  // primitives
+  var result = obj;
+  if(typeof result==='string'){
+    return "\""+result+"\"";
+  }else if(typeof result==='number' || result===null || typeof result==='boolean' ){
+    return String(result);
+  }else if(typeof result===undefined){
+    return;
+  }
+  //Arrays
+  if(result.constructor===Array){
+    if(result.length===0){ 
+      return '[]';
+    }
+    result = result.reduce(function(acc, item, i) {
+      if(i===result.length-1){
+        return acc = acc.concat(stringifyJSON(item));
+      }else{
+        return acc = acc.concat(stringifyJSON(item) + ",");
+      }
+    }, '[');
+    return result+"]";
+  }
+
+  var keys = Object.keys(result);
+  if(keys.length === 0) {
+    return '{}';
+  }
+  var ans="";
+  for(var k in result){
+    if(typeof result[k]==='function' || typeof result[k]==='undefined'){
+      return "{}";
+    } 
+    var val = stringifyJSON( result[k] )
+    var key = stringifyJSON( k );
+    ans = ans.concat(key+ ":" + val+",");
+  }
+  return ans = "{" + ans.substring(0,ans.length-1) + "}";
   // your code goes here
 };
